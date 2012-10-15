70fbb3c3e48fb5f5b21635d076d6e49374cdc525
Manuel Palacio <emanuel.palacio@gmail.com>
2012-10-15 18:28:54 +0200

    fixed data import

--- src/main/groovy/net/palacesoft/neo4j/movies/MovieResource.groovy
+++ src/main/groovy/net/palacesoft/neo4j/movies/MovieResource.groovy
@@ -40,37 +40,38 @@ class MovieResource {
             }
         }
 
-        def script = """g.setMaxBufferSize(1000)
+        def script = """g.setMaxBufferSize(1000);
                         '${localhost}/movies.dat'.toURL().eachLine { def line ->
-                             def components = line.split('::')
-                             def movieVertex = g.addVertex(['type':'Movie', 'movieId':components[0].toInteger(), 'title':components[1]])
-                               components[2].split('|').each { def genera ->
-                                def hits = g.idx(Tokens.T.v)[[genera:genera]].iterator() def generaVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Genera', 'genera':genera])
-                                g.addEdge(movieVertex, generaVertex, 'hasGenera')
+                             def components = line.split('::');
+                             def movieVertex = g.addVertex(['type':'Movie', 'movieId':components[0].toInteger(), 'title':components[1]]);
+                             components[2].split('|').each { def genera ->
+                                def hits = g.idx(Tokens.T.v)[[genera:genera]].iterator();
+                                def generaVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Genera', 'genera':genera]);
+                                g.addEdge(movieVertex, generaVertex, 'hasGenera');
                                 }
                              }
                              occupations = [0:'other', 1:'academic/educator', 2:'artist', 3:'clerical/admin', 4:'college/grad student', 5:'customer service',
                                       6:'doctor/health care', 7:'executive/managerial', 8:'farmer',
                                       9:'homemaker', 10:'K-12 student', 11:'lawyer', 12:'programmer',
                                        13:'retired', 14:'sales/marketing', 15:'scientist', 16:'self-employed',
-                                       17:'technician/engineer', 18:'tradesman/craftsman', 19:'unemployed', 20:'writer']
+                                       17:'technician/engineer', 18:'tradesman/craftsman', 19:'unemployed', 20:'writer'];
 
                                      '${localhost}/users.dat'.toURL().eachLine { def line ->
-                                       def components = line.split('::')
-                                       def userVertex = g.addVertex(['type':'User', 'userId':components[0].toInteger(), 'gender':components[1], 'age':components[2].toInteger()])
-                                       def occupation = occupations[components[3].toInteger()]
-                                       def hits = g.idx(Tokens.T.v)[[occupation:occupation]].iterator()
-                                       def occupationVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Occupation', 'occupation':occupation])
-                                       g.addEdge(userVertex, occupationVertex, 'hasOccupation')
+                                       def components = line.split('::');
+                                       def userVertex = g.addVertex(['type':'User', 'userId':components[0].toInteger(), 'gender':components[1], 'age':components[2].toInteger()]);
+                                       def occupation = occupations[components[3].toInteger()];
+                                       def hits = g.idx(Tokens.T.v)[[occupation:occupation]].iterator();
+                                       def occupationVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Occupation', 'occupation':occupation]);
+                                       g.addEdge(userVertex, occupationVertex, 'hasOccupation');
                                      }
 
                                      '${localhost}/ratings.dat'.toURL().eachLine {def line ->
-                                       def components = line.split('::')
-                                       def ratedEdge = g.addEdge(g.idx(Tokens.T.v)[[userId:components[0].toInteger()]].next(), g.idx(T.v)[[movieId:components[1].toInteger()]].next(), 'rated')
-                                       ratedEdge.setProperty('stars', components[2].toInteger())
+                                       def components = line.split('::');
+                                       def ratedEdge = g.addEdge(g.idx(Tokens.T.v)[[userId:components[0].toInteger()]].next(), g.idx(T.v)[[movieId:components[1].toInteger()]].next(), 'rated');
+                                       ratedEdge.setProperty('stars', components[2].toInteger());
                                        }
 
-                                      g.stopTransaction(TransactionalGraph.Conclusion.SUCCESS)"""
+                                      g.stopTransaction(TransactionalGraph.Conclusion.SUCCESS);"""
 
         neo4jTemplate.execute(script, null)
 
