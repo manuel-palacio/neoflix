36a90f48d07dd503db35e996bd52741186e30f6b
Manuel Palacio <emanuel.palacio@gmail.com>
2012-11-10 16:55:30 +0100

    servlet to create graph

--- src/main/groovy/net/palacesoft/neo4j/movies/MovieResource.groovy
+++ src/main/groovy/net/palacesoft/neo4j/movies/MovieResource.groovy
@@ -1,19 +1,17 @@
 package net.palacesoft.neo4j.movies
 
 import com.sun.jersey.core.spi.factory.ResponseBuilderImpl
-import com.sun.jersey.spi.resource.Singleton
+import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 import org.neo4j.rest.graphdb.entity.RestNode
 import org.springframework.data.neo4j.core.GraphDatabase
 import org.springframework.data.neo4j.rest.SpringRestGraphDatabase
 import org.springframework.data.neo4j.support.Neo4jTemplate
 
-import javax.annotation.PostConstruct
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.Response
-import groovy.json.JsonBuilder
 
 @Path("/show")
 class MovieResource {
@@ -25,62 +23,6 @@ class MovieResource {
 
     GraphDatabase graphDb = new SpringRestGraphDatabase(neoUrl)
 
-    def host = "http://neoflix-groovy.herokuapp.com"
-
-    @PostConstruct
-    void createGraphIfNeeded() {
-
-        def neo4jTemplate = new Neo4jTemplate(graphDb)
-
-
-        if (neo4jTemplate.execute("g.idx('vertices')[[type:'Movie']].count()", null).to(Integer.class).single() > 0) {
-            return
-        }
-
-
-        if (neo4jTemplate.execute("g.indices", null).toList().empty) {
-            neo4jTemplate.execute("g.createAutomaticIndex('vertices', Vertex.class, null)", null)
-            if (neo4jTemplate.execute("g.V.count()", null).to(Integer.class).single() > 0) {
-                neo4jTemplate.execute("AutomaticIndexHelper.reIndexElements(g, g.idx('vertices'), g.V)", null)
-            }
-        }
-
-        def script = """g.setMaxBufferSize(1000);
-                        '${host}/movies.dat'.toURL().eachLine { def line ->
-                             def components = line.split('::');
-                             def movieVertex = g.addVertex(['type':'Movie', 'movieId':components[0].toInteger(), 'title':components[1]]);
-                             components[2].split('|').each { def genera ->
-                                def hits = g.idx(Tokens.T.v)[[genera:genera]].iterator();
-                                def generaVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Genera', 'genera':genera]);
-                                g.addEdge(movieVertex, generaVertex, 'hasGenera');
-                                }
-                             }
-                             occupations = [0:'other', 1:'academic/educator', 2:'artist', 3:'clerical/admin', 4:'college/grad student', 5:'customer service',
-                                      6:'doctor/health care', 7:'executive/managerial', 8:'farmer',
-                                      9:'homemaker', 10:'K-12 student', 11:'lawyer', 12:'programmer',
-                                       13:'retired', 14:'sales/marketing', 15:'scientist', 16:'self-employed',
-                                       17:'technician/engineer', 18:'tradesman/craftsman', 19:'unemployed', 20:'writer'];
-
-                                     '${host}/users.dat'.toURL().eachLine { def line ->
-                                       def components = line.split('::');
-                                       def userVertex = g.addVertex(['type':'User', 'userId':components[0].toInteger(), 'gender':components[1], 'age':components[2].toInteger()]);
-                                       def occupation = occupations[components[3].toInteger()];
-                                       def hits = g.idx(Tokens.T.v)[[occupation:occupation]].iterator();
-                                       def occupationVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Occupation', 'occupation':occupation]);
-                                       g.addEdge(userVertex, occupationVertex, 'hasOccupation');
-                                     }
-
-                                     '${host}/ratings.dat'.toURL().eachLine {def line ->
-                                       def components = line.split('::');
-                                       def ratedEdge = g.addEdge(g.idx(Tokens.T.v)[[userId:components[0].toInteger()]].next(), g.idx(T.v)[[movieId:components[1].toInteger()]].next(), 'rated');
-                                       ratedEdge.setProperty('stars', components[2].toInteger());
-                                       }
-
-                                      g.stopTransaction(TransactionalGraph.Conclusion.SUCCESS);"""
-
-        neo4jTemplate.execute(script, null)
-
-    }
 
     private def getRecommendations(long id) {
         def neo4jTemplate = new Neo4jTemplate(graphDb)
@@ -118,8 +60,6 @@ class MovieResource {
         }
 
         return [id: id, name: "Recommendations", values: jsonResult]
-
-
     }
 
     @GET
@@ -163,6 +103,5 @@ class MovieResource {
 
 
         "<a href='${movieUrl}' target='_blank'><img src='${poster}'><h3>${tagLine}</h3><p>Rating: ${rating} <br/>Rated: ${certification}</p><p>${overview}</p>"
-
     }
 }
