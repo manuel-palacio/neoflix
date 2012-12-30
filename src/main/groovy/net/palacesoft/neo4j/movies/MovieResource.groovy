5071881e6662bd81633b83553ccd764166345da8
Manuel Palacio <emanuel.palacio@gmail.com>
2012-12-30 10:06:05 +0100

    rollback tmdb api

--- src/main/groovy/net/palacesoft/neo4j/movies/MovieResource.groovy
+++ src/main/groovy/net/palacesoft/neo4j/movies/MovieResource.groovy
@@ -2,19 +2,18 @@ package net.palacesoft.neo4j.movies
 
 import com.sun.jersey.core.spi.factory.ResponseBuilderImpl
 import groovy.json.JsonBuilder
-import net.palacesoft.tmdb.TmdbMovie
-import net.palacesoft.tmdb.model.Movie
+import groovy.json.JsonSlurper
 import org.neo4j.rest.graphdb.entity.RestNode
 import org.springframework.data.neo4j.core.GraphDatabase
 import org.springframework.data.neo4j.rest.SpringRestGraphDatabase
 import org.springframework.data.neo4j.support.Neo4jTemplate
 
-import javax.annotation.PostConstruct
 import javax.ws.rs.GET
 import javax.ws.rs.Path
-import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
 import javax.ws.rs.core.Response
+import javax.annotation.PostConstruct
+import javax.ws.rs.Produces
 
 @Path("/show")
 class MovieResource {
@@ -97,22 +96,25 @@ class MovieResource {
     }
 
     private def getPoster(RestNode node) {
+        def movieResponse = new JsonSlurper()
+
+        def result = movieResponse.parseText(new URL("http://api.themoviedb.org/3/search/movie?api_key=${movieKey}&query=${URLEncoder.encode(node.getProperty("title").toString())}").text)
+
+        def movieUrl = "http://www.themoviedb.org/movie/${result.results.id[0]}"
+        def poster = "http://cf2.imgobject.com/t/p/w185${result.results.poster_path[0]}"
+        def rating = result.results.vote_average[0]
+
         try {
-            TmdbMovie tmdbMovie = new TmdbMovie(movieKey)
-            Movie movie = tmdbMovie.search(node.getProperty("title").toString(), 1)[0]
-
-            def movieUrl = "http://www.themoviedb.org/movie/${movie.id}"
-            def poster = tmdbMovie.getPosterUrlForSize(movie.id, "w185")
-            def rating = movie.vote_average
-            def tagLine = movie.tagline
-            def overview = movie.overview
-            def certification = movie.status
-
+            def details = movieResponse.parseText(new URL("http://api.themoviedb.org/3/movie/${result.results.id[0]}?api_key=${movieKey}").text)
+            def tagLine = details.tagline
+            def certification = ""
+            def overview = details.overview
             return "<a href='${movieUrl}' target='_blank'><img src='${poster}'><h3>${tagLine}</h3><p>Rating: ${rating} <br/>Rated: ${certification}</p><p>${overview}</p>"
         } catch (e) {
             //ignore
         }
 
         return ""
+
     }
 }
